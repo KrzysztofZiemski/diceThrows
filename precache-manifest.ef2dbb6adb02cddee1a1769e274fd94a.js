96d483c09674990b2051f268c459adedc6c33d3b
Krzysiek Ziemski <krzysztofziemski@wp.pl>
2019-09-08 18:32:02 +0200

    Updates

---
+++ precache-manifest.ef2dbb6adb02cddee1a1769e274fd94a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2881e9f3bcddc5e83d27ce4c989c74c",
+    "url": "/dicesThrows/index.html"
+  },
+  {
+    "revision": "efe064d641f27ed0a4cf",
+    "url": "/dicesThrows/static/css/main.76b4ed63.chunk.css"
+  },
+  {
+    "revision": "f0f6ffd1818a6fff80cb",
+    "url": "/dicesThrows/static/js/2.5308031a.chunk.js"
+  },
+  {
+    "revision": "efe064d641f27ed0a4cf",
+    "url": "/dicesThrows/static/js/main.4e9b95d9.chunk.js"
+  },
+  {
+    "revision": "9546deccd2a49d30853a",
+    "url": "/dicesThrows/static/js/runtime~main.9d3ca9f3.js"
+  },
+  {
+    "revision": "fc91832e5012cfd609e93efd948dcf6f",
+    "url": "/dicesThrows/static/media/attack.fc91832e.png"
+  },
+  {
+    "revision": "73305845e032d1504d1b683545118e3b",
+    "url": "/dicesThrows/static/media/d10-icon.73305845.png"
+  },
+  {
+    "revision": "ec6df3a9ea5e8112fa4078fc772c43d5",
+    "url": "/dicesThrows/static/media/d12-icon.ec6df3a9.png"
+  },
+  {
+    "revision": "25a04458cecb9094bcb87d5ed15916a8",
+    "url": "/dicesThrows/static/media/d20-icon.25a04458.png"
+  },
+  {
+    "revision": "cdad945b7884058a39a3655ab0d7b5f7",
+    "url": "/dicesThrows/static/media/d4-icon.cdad945b.png"
+  },
+  {
+    "revision": "226340d91cf1134dcd887250eff4a55e",
+    "url": "/dicesThrows/static/media/d8-icon.226340d9.png"
+  },
+  {
+    "revision": "eb6ffb459f72850c0f536a0253abf3ab",
+    "url": "/dicesThrows/static/media/dices-roll.eb6ffb45.png"
+  },
+  {
+    "revision": "942b53acb451d25df0153d5aed42ec53",
+    "url": "/dicesThrows/static/media/dndLogo.942b53ac.png"
+  },
+  {
+    "revision": "fbdfcf321444e2949eabf1a45641ed14",
+    "url": "/dicesThrows/static/media/history2.fbdfcf32.png"
+  },
+  {
+    "revision": "3070a8a04415e6592ad962af0e58a885",
+    "url": "/dicesThrows/static/media/logoDnd.3070a8a0.png"
+  },
+  {
+    "revision": "2c372b4c424eff4c044e4075b5cc7b03",
+    "url": "/dicesThrows/static/media/select.2c372b4c.png"
+  },
+  {
+    "revision": "eb147918916a617a86eb31007409e7ef",
+    "url": "/dicesThrows/static/media/unselect.eb147918.png"
+  }
+]);

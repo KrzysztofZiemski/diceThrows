24dd5289180ea9882b6346f128ac301b06d38d0e
Krzysiek Ziemski <krzysztofziemski@wp.pl>
2019-09-09 08:24:20 +0200

    add Error Page

--- src/modules/pages/ErrorPage.js
+++ src/modules/pages/ErrorPage.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom'
+import '../../styles/ErrorPage.css'
 const ErrorPage = () => {
-    return (<div>error</div>);
+    return (<div className="ErrorPage">
+        <h1>Strona nie znaleziona</h1>
+        <p>Wróć do strony głównej i spróbuj ponownie</p>
+        <div><NavLink to="/" exact>Strona główna</NavLink></div>
+    </div>);
 }
 
 export default ErrorPage;
